e5fcfdc5562e7ed5414585b28cd9f4322b5e5d97
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-11-28 22:35:42 +0600

    form logic added

--- src/Pages/SellProduct.js
+++ src/Pages/SellProduct.js
@@ -7,14 +7,28 @@ const SellProduct = () => {
 
     const InsertData = (e) => {
         e.preventDefault()
+        const event = e.target
+        const user = e.target.user.value
+        const email = e.target.email.value
+        const name = e.target.name.value
+        const location = e.target.location.value
+        const used = e.target.used.value
+        const date = e.target.date.value
+        const originPrice = e.target.originPrice.value
+        const price = e.target.price.value
+        const type = e.target.type.value
+        const number = e.target.number.value
+        const image = e.target.image.value
+        const product = { user, email, price, type, number, name, location, used, originPrice, date, image }
+        console.log(product)
     }
     return (
         <div className='pt-36 pb-10'>
             <form className='w-6/12 m-auto bg-indigo-300 p-6' onSubmit={InsertData}>
-                <input type="text" defaultValue={user.displayName} disabled className="input input-bordered w-full " />
+                <input type="text" name='user' defaultValue={user.displayName} disabled className="input input-bordered w-full " />
                 <br />
                 <br />
-                <input type="text" defaultValue={user.email} disabled className="input input-bordered w-full " />
+                <input type="text" name='email' defaultValue={user.email} disabled className="input input-bordered w-full " />
                 <br />
                 <br />
                 <hr className='hero' />
@@ -32,13 +46,13 @@ const SellProduct = () => {
                 <input type="number" placeholder="Type here" required name='price' className="input input-bordered w-full " />
                 <p className='font-bold'>Selling Type:</p>
                 <select className="select select-ghost w-full ">
-                    <option disabled selected>Pick the best JS framework</option>
+                    <option name='type' disabled selected>Pick the best JS framework</option>
                     <option>processor</option>
                     <option>casing</option>
                     <option>ram</option>
                 </select>
                 <p className='font-bold'>Image URL:</p>
-                <input type="number" placeholder="Type here" required name='price' className="input input-bordered w-full " />
+                <input type="text" placeholder="Type here" required name='image' className="input input-bordered w-full " />
                 <br />
                 <br />
                 <div className='flex items-center justify-center'>
